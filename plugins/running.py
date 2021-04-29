66945ffa80a5bb4dbd271d4f14cb7cffe9da626d
Amal [Devoloper.TG] <65230767+DevDevolopertg@users.noreply.github.com>
2021-04-29 21:26:00 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -42,6 +42,7 @@ async def run(bot, message):
                 chat_id=TO,
                 from_chat_id=FROM,       
                 caption=Translation.CAPTION.format(file_name),
+                parse_mode="md",
                 message_id=message.message_id
             )
             files_count += 1
